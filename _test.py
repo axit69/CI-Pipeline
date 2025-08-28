dec3ed930d9f9b92e046430f80659ac2d5e9a50d
axitmehta <axitmehta6@gmail.com>
2025-08-28 12:09:42 +0530

    Ci implemented

--- _test.py
+++ _test.py
@@ -15,11 +15,11 @@ def test_square():
 
 def test_cube():
     assert cube(2) == 8,        "Test failed: cube of 2 should be 8"
-    assert square(3) == 27,      "Test failed: cube of 3 should be 27"
+    assert cube(3) == 27,      "Test failed: cube of 3 should be 27"
 
 def test_fifth_power():
     assert fifth_power(2) == 32,        "Test failed: cube of 2 should be 32"
-    assert square(3) == 243,            "Test failed: cube of 3 should be 243"
+    assert fifth_power(3) == 243,            "Test failed: cube of 3 should be 243"
 
 
 
